42e44ab8dc0d3a372631f069a35ed3b4cbbe74ef
yulka <yupostupinskaya@sdvor.com>
2022-10-14 23:01:04 +0500

    Получение начальных данных

--- shared/src/commonMain/kotlin/kmmktor/ClientKtor.kt
+++ shared/src/commonMain/kotlin/kmmktor/ClientKtor.kt
@@ -1,6 +1,7 @@
 package kmmktor
 
 import io.ktor.client.*
+import io.ktor.client.call.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 
@@ -8,9 +9,18 @@ class ClientKtor {
 
     private val client = HttpClient()
     private val homeDirectory = "http://109.194.162.125/debit72/hs/debit72/"
+    private val info = "PostJSON"
+    suspend fun generalInformation(): GeneralInformation {
+        val response = client.get(homeDirectory + info)
+        return response.body<GeneralInformation>()
+    }
 
     suspend fun greeting(): String {
         val response = client.get("https://ktor.io/docs/")
         return response.bodyAsText()
     }
 }
+
+data class GeneralInformation(
+    val name: String
+)
