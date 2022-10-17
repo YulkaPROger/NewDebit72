3393b39f94db454c169b67fa94b3a3bcfa39dc00
yulka <yupostupinskaya@sdvor.com>
2022-10-17 23:05:00 +0500

    Сериализация из русских неймингов

--- shared/src/commonMain/kotlin/model/GeneralInformation.kt
+++ shared/src/commonMain/kotlin/model/GeneralInformation.kt
@@ -1,14 +1,15 @@
 package model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class GeneralInformation(
-    val count: String,
-    val balanceOwed: String,
-    val totalDebt: String,
-    val amountOfDuty: String,
-    val amountOfLegalServices: String,
-    val amountPenalty: String,
-    val amountOfCommunalServices: String
+    @SerialName("Количество") val count: String,
+    @SerialName("ОстатокДолга") val balanceOwed: String,
+    @SerialName("ОбщаяСуммаДолга") val totalDebt: String,
+    @SerialName("СуммаПошлины") val amountOfDuty: String,
+    @SerialName("СуммаЮрУслуг") val amountOfLegalServices: String,
+    @SerialName("СуммаПени") val amountPenalty: String,
+    @SerialName("СуммаВзысканияЖКУ") val amountOfCommunalServices: String
 )
